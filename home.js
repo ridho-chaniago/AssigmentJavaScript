7c646496e9a3ded16767103ce7ba4eec0f19c742
Ridho-Chaniago <ridho12c.rc@gmail.com>
2023-10-19 15:05:17 +0700

    1

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+// import {clear} from './clear.js'
+
 function getDataUser() {
     //1.get data localstorage
     //2.Convert data ke object
@@ -23,18 +25,54 @@ function getDataUser() {
 getDataUser()
 
 
-function onLogOut(){
-    // localStorage.removeItem('user')
-    window.location.href = './index.html'
+
+
+let price = document.getElementById('price')
+let cost = document.getElementById('cost')
+let quantity = document.getElementById('quantity')
+let amount = document.getElementById('amount')
+console.log(typeof total)
+function changeQuantity(){
+    var total= parseInt(price.value)*parseInt(quantity.value)
+    console.log(total)
+    if(total>=50000 || quantity.value==0){
+        cost.value=0
+        amount.value=total
+    }else{
+        cost.value=10000
+        amount.value=total+parseInt(cost.value)
+    }
 }
+console.log(typeof price.value)
+function changeProduct(){
+    const selectProduct = document.getElementById('product2').value
+    console.log(selectProduct)
+        const olshop =document.getElementById('olshop').value
+        const ultah =document.getElementById('ultah').value
+        const aqiqah =document.getElementById('aqiqah').value
+    if (selectProduct==olshop){
+        price.value=10000
+        cost.value = 10000
+       
+        changeQuantity()
+    }else if (selectProduct==ultah){
+        price.value=12000
+        cost.value=10000
+        changeQuantity()
+    }else if (selectProduct==aqiqah){
+        price.value=15000
+        cost.value=10000
+        changeQuantity()}
+}
+document.getElementById('product2')
+console.log
+
+
+
 
 function input(){
     const product1 = document.getElementById('product1').value
     const product2 = document.getElementById('product2').value
-        const olshop =document.getElementById('olshop').value
-        console.log(olshop)
-        const ultah =document.getElementById('ultah')
-        const aqiqah =document.getElementById('Aqiqah')
     const price = document.getElementById('price').value
     const quantity = document.getElementById('quantity').value
     const cost = document.getElementById('cost').value
@@ -56,7 +94,23 @@ function input(){
         cost : cost,
         amount : amount
     }
-    cell1.innerHTML=new Date()
+
+        let today=new Date()
+        let dayname = {weekday: 'long'}
+        let day = today.toLocaleString('id-ID', dayname);
+        let date = today.getDate()
+        let month = today.getMonth()+1
+        let year =today.getFullYear()
+        if(date<10){
+            date = "0"+date
+        }
+        if(month<10){
+            month='0' + month
+        }
+        let formatDate = `${day}, ${date}-${month}-${year}`
+
+    
+    cell1.innerHTML=formatDate
     if(product1){
         cell2.innerHTML=prodList.product1
     }else{
@@ -69,6 +123,10 @@ function input(){
     cell6.innerHTML=amount
     console.log(prodList)
 }
+function onLogOut(){
+    // localStorage.removeItem('user')
+    window.location.href = './index.html'
+}
 function clear(){
     document.getElementById('product1').value=""
     document.getElementById('product2').value=""
@@ -77,7 +135,22 @@ function clear(){
     document.getElementById('cost').value=""
     document.getElementById('amount').value=""
 }
+function validate(){
+   let a= document.getElementById('product2').value
+  let b=  document.getElementById('price').value
+  let c=  document.getElementById('quantity').value
+  let d=  document.getElementById('cost').value
+  let e=  document.getElementById('amount').value
+    if (a && b && c ){
+        console.log('oke')
+        input()
+        clear()
+    }else{
+        alert('input tidak boleh kosong')
+    }
+    console.log(a, b ,c)
+}
 const btnSubmit = document.getElementById('btnSubmit')
-btnSubmit.addEventListener('click', ()=> {input(), clear()})
+btnSubmit.addEventListener('click', ()=> { validate()})
 const btnCancel = document.getElementById('btnCancel') 
 btnCancel.addEventListener('click', ()=>{clear()})
